679fc55157abf631173c8a7fa51672863af44844
xieq <727170890@qq.com>
2023-07-18 14:56:38 +0800

    first commit

---
+++ vite-electron-vue3-ts/electron/main.ts
@@ -0,0 +1,54 @@
+const { app, BrowserWindow } = require('electron')
+let win
+ 
+/**
+ * @Description: electron程序入口
+ * @Author: Etc.End
+ * @Copyright: TigerSong
+ * @CreationDate 2023-05-20 14:39:26
+ */
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        minWidth: 1200,
+        minHeight: 800,
+        center: true,
+        skipTaskbar: false,
+        transparent: false,
+        webPreferences: {
+            contextIsolation: false,
+            webSecurity: false,
+        }
+    })
+ 
+    win.loadURL(
+        'http://localhost:5173/'
+    )
+ 
+    win.webContents.openDevTools()
+}
+ 
+app.whenReady().then(() => {
+    createWindow()
+})
+ 
+/**
+ * @Description: 限制只能打开一个页面
+ * @CreationDate 2023-05-20 14:35:52
+ */
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        if (win) {
+            if (win.isMinimized()) win.restore()
+            win.focus()
+        }
+    })
+}
+ 
+app.on('window-all-closed', function () {
+    if(process.platform !== 'darwin') app.quit()
+})
